6c13d61e504f2150dc530852060050387c6d7423
boydh4 <boydh4@mcmaster.ca>
2023-11-24 16:48:54 -0500

    Gamemechs 1B Partially Done

--- Project.cpp
+++ Project.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
+#include "Player.h"
 
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+GameMechs* myGM;
+Player* myPlayer
 
 void Initialize(void);
 void GetInput(void);
 void RunLogic(void);
 void DrawScreen(void);
 void LoopDelay(void);
+
 void CleanUp(void);
 
 
@@ -23,7 +27,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(myGM->getExitFlagStatus() == false)  
     {
         GetInput();
         RunLogic();
@@ -41,24 +45,66 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    myGM = new GameMechs(30, 15);
+    myPlayer = new Player(myGM);
 }
 
 void GetInput(void)
 {
-   
+    myGM -> getInput();
 }
 
 void RunLogic(void)
 {
-    
+    myPlayer -> updatePlayerDir();
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
 
+    
+    for (int y = 0; y < ROWS; y++)
+    {
+        for (int x = 0; x < COLS; x++)
+        {
+            if (y == 0 || y == ROWS - 1)
+            {
+                if (x < COLS - 1)
+                {
+                    MacUILib_printf("#");
+                }
+                else
+                {
+                    MacUILib_printf("#\n");
+                }
+            }
+
+            else if ((y > 0) && (y < COLS - 1))
+            {
+                if (x == 0)
+                {
+                    MacUILib_printf("#");
+                }
+                else if (x == COLS - 1)
+                {
+                    MacUILib_printf("#\n");
+                }
+                else
+                {
+                    MacUILib_printf(" ");
+                }
+            }
+        }
+    }
+    objPos tempPos;
+    myPlayer->getPlayerPos(tempPos);
+    MacUILib_printf("Board Size: %dx%d, Player Pos: <%d, %d> + %c\n", myGM->getBoardSizeX()
+    , myGM->getBoardSizeY(),
+     tempPos.x, tempPos.y, tempPos.symbol );
 }
+
+  
 
 void LoopDelay(void)
 {
